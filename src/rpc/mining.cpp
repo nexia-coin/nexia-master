298d4e97362efbd645a5b29c0efda83ca9d8e91b
Jari478 <258359983+Jari478@users.noreply.github.com>
2026-02-18 14:07:22 -0500

    mweb rpc

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -727,8 +727,8 @@ static RPCHelpMan getblocktemplate()
     }
 
     // GBT must be called with 'segwit' and 'mweb' sets in the rules
-    if (setClientRules.count("segwit") != 1) {
-        throw JSONRPCError(RPC_INVALID_PARAMETER, "getblocktemplate must be called with the segwit & mweb rule sets (call with {\"rules\": [\"segwit\"]})");
+    if (setClientRules.count("segwit") != 1 || setClientRules.count("mweb") != 1) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "getblocktemplate must be called with the segwit & mweb rule sets (call with {\"rules\": [\"mweb\", \"segwit\"]})");
     }
 
     // Update block
